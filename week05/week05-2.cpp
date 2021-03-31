3df4cb1ad6ba0f23710d3db37b7f04e9826f7aa8
chendooou <79628426+chendooou@users.noreply.github.com>
2021-03-31 10:58:49 +0800

    Add files via upload

---
+++ week05/week05-2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char line[10]="decline";
+    char line2[20]={'p','r','o','p','e','r','\0'};
+
+    printf("%s\n",line);
+    scanf("%s\n",line2);
+
+    char line3[]="majority這是好的，沒問題，要多長有多長";
+    char line4[]={'m','a','j','o','r','i','t','y'};
+    printf("%s\n",line3);
+
+    printf("你相信嗎? 這是 line4:===%s==\n",line4);
+
+
+}
+
